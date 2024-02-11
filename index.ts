d49d935f4f39b78a94744a7f1942ed1bfe08857a
98.derin <98.derin@gmail.com>
2024-02-12 03:13:15 +0530

    initial commit

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
 console.log("Hello via Bun!");
+console.log("Hello via Bun!");
+
+const read = (text: string) => {
+    return text
+}
+
+const msg: string = read ("Hello world")
+
+console.log(msg)
