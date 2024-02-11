3290e50ac8d401719d75a68020e605ff8b4b07f3
98.derin <98.derin@gmail.com>
2024-02-12 03:50:46 +0530

    Added routes

--- index.ts
+++ index.ts
@@ -1,10 +1,33 @@
-console.log("Hello via Bun!");
-console.log("Hello via Bun!");
+import figlet from "figlet";
 
-const read = (text: string) => {
-    return text
-}
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        const url = new URL(req.url);
 
-const msg: string = read ("Hello world")
+        if (url.pathname === '/') {
+            const body = figlet.textSync("I am learning bun");
+            return new Response(body);
+        }
+        if (url.pathname === '/aboutMe') {
+            return new Response("About me");
+        }
+        if (url.pathname === '/greet') {
+            return new Response(Bun.file('./greet.txt'));
+        }
+        if (url.pathname === '/error') {
+            throw new Error("Could not fetch data.");
+        }
+        return new Response('404!');
+    },
+    error(error){
+        return new Response(`<pre> ${error} \n ${error.stack} </pre>`,{
+            headers:{
+                'Content-Type': 'text/html'
+            }
+        })
 
-console.log(msg)
+    },
+
+});
+console.log("on port 3000")
